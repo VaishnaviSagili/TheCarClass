49a56c1003b36f9d19ff7ef46c440b59d8feccc5
VaishnaviSagili <lolbeans.io667@gmail.com>
2026-09-12 20:22:03 -0400

    creating car objects

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Car car1 = new Car("Toyota", "Camry", 14, 32);
+        Car car2 = new Car("Ford", "F-150", 26, 20);
+
+        car1.drive(200);
+        car1.refuel(5);
+
+        car2.drive(150);
+        System.out.println(car2.getMake() + " " + car2.getModel() + " has " + car2.getCurrentFuel() + " gallons left.");
+
+
+
     }
 }
